19ca556de9f0100509ce1e26ae2561deacea3c2e
Theodore Tsitsimis <th.tsitsimis@gmail.com>
2020-09-01 11:11:06 +0300

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="disjunctive-nn",
-    version="0.1.0",
+    version="0.1.1",
     description="PyTorch implementation of Disjunctive Normal Networks",
     long_description=readme(),
     long_description_content_type="text/markdown",
